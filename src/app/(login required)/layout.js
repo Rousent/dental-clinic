97abfa55e8071ffef4747dcf4ca7310785146a7f
Rousent <86386444+Rousent@users.noreply.github.com>
2023-12-13 20:42:01 -0600

    Redirect sin session a login

--- src/app/(login required)/layout.js
+++ src/app/(login required)/layout.js
@@ -1,13 +1,20 @@
-"use client"
-import Navigation from "@/components/Navbar"
+import Navigation from "@/components/Navbar";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 export default async function NavigationLayout({ children }) {
-    return (
-        <>
-        <Navigation/>
-        <div className="max-w-[1024px] mx-auto px-10">
-            { children }
-        </div>
-        </>
-    )
+	const supabase = createServerComponentClient({ cookies });
+	const {
+		data: { session },
+	} = await supabase.auth.getSession();
+
+	if (!session) redirect("/login");
+
+	return (
+		<>
+			<Navigation />
+			<div className="max-w-[1024px] mx-auto px-10">{children}</div>
+		</>
+	);
 }
---
+++ src/app/login/page.js
@@ -0,0 +1,7 @@
+export default function Login() {
+	return (
+		<div className="w-fit h-fit">
+			<div>Login Page</div>
+		</div>
+	);
+}
